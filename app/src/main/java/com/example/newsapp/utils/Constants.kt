93d42aca0bfc9013fcc31fa9cdca740acbcbd3d3
mhmood_adel <40132973+Malekel3alamy@users.noreply.github.com>
2024-09-11 18:39:30 +0300

    Update Constants.kt

--- app/src/main/java/com/example/newsapp/utils/Constants.kt
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.example.newsapp.utils
 class Constants {
     companion object{
 
-        const val API_KEY = "pub_518400b887b5dac7b523cd7c56883f2096729"
+        const val API_KEY = "put your api here "
        // const val API_KEY = "d3a82e89907f4bd8a4743e9d1613a686"
         const val BASE_URL = "https://newsdata.io/api/1/"
         const val SEARCH_NEWS_TIME_DELAY = 4000L
